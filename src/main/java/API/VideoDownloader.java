b53de62b3e4a03a97220703064c11b761758591b
João Mourão Sá <joao.mourao.sa@gmail.com>
2022-01-09 04:38:48 +0000

    added todos and stuff

--- src/main/java/API/VideoDownloader.java
+++ src/main/java/API/VideoDownloader.java
@@ -1,43 +1,74 @@
 package API;
 
-import com.github.kiulian.downloader.model.videos.formats.Format;
+import com.github.kiulian.downloader.*;
+import com.github.kiulian.downloader.downloader.YoutubeCallback;
+import com.github.kiulian.downloader.downloader.YoutubeProgressCallback;
+import com.github.kiulian.downloader.downloader.request.RequestVideoFileDownload;
+import com.github.kiulian.downloader.downloader.request.RequestVideoInfo;
+import com.github.kiulian.downloader.downloader.response.Response;
+import com.github.kiulian.downloader.model.videos.VideoDetails;
+import com.github.kiulian.downloader.model.videos.VideoInfo;
+import com.github.kiulian.downloader.model.videos.formats.VideoFormat;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeoutException;
 
-import API.JavaYoutubeDownloader;
-import API.VideosDownloader;
 
 public class VideoDownloader {
 
     private static void createFolder(String path) throws IOException {
-        Path path_ = Paths.get(path);
-        Files.createDirectory(path_);
+        Files.createDirectory(Paths.get(path));
     }
 
-    public static void main(String[] args) throws TimeoutException {
+    public static void download(String link, String path) throws TimeoutException {
+        YoutubeDownloader downloader = new YoutubeDownloader();
+        String videoId = link; // for url https://www.youtube.com/watch?v=abc12345
 
-        String link = "JmXjYi7FOVQ";
-        String path = "videos";
-        String channelId = "ValerioVelardoTheSoundofAI";
+        RequestVideoInfo request = new RequestVideoInfo(videoId).callback(new YoutubeCallback<VideoInfo>() {
+            @Override
+            public void onFinished(VideoInfo videoInfo) {
+                System.out.println("Finished parsing");
+            }
 
-        try {
-            createFolder(path);
-        }
-        catch (IOException e) {
-            System.err.println("Failed to create directory " + e.getMessage());
-        }
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("Error: " + throwable.getMessage());
+            }
+        }).async();
+        Response<VideoInfo> response = downloader.getVideoInfo(request);
+        VideoInfo video = response.data(); // will block thread
 
-        VideosDownloader.getAllVideos(channelId);
+        VideoDetails details = video.details();
+        System.out.println(details.title());
 
-        //JavaYoutubeDownloader.run(link, path);
+        String video_name = details.title().replaceAll(" ", "_").toLowerCase();
 
+        VideoFormat bestFormat = video.bestVideoWithAudioFormat();
+        RequestVideoFileDownload request2 = new RequestVideoFileDownload(bestFormat)
+                .saveTo(new File(path)) // by default "videos" directory
+                .renameTo(video_name) // by default file name will be same as video title on youtube
+                .overwriteIfExists(true) // if false and file with such name already exits sufix will be added video(1).mp4
+                .callback(new YoutubeProgressCallback<File>() {
+                    @Override
+                    public void onDownloading(int progress) {
+                        System.out.printf("Downloaded %d%%\n", progress);
+                    }
 
+                    @Override
+                    public void onFinished(File videoInfo) {
+                        System.out.println("Finished file: " + videoInfo);
+                    }
 
+                    @Override
+                    public void onError(Throwable throwable) {
+                        System.out.println("Error: " + throwable.getLocalizedMessage());
+                    }
+                })
+                .async();
+        Response<File> response2 = downloader.downloadVideoFile(request2);
+        File data = response2.data(); // will block current thread
     }
 }
--- src/main/java/API/VideosDownloader.java
+++ src/main/java/API/VideosDownloader.java
@@ -38,14 +38,41 @@ public class VideosDownloader {
         return output;
     }
 
+    public static void downloadVideo(String video) {
+
+    }
 
     public static void downloadVideos(List<String> list) throws TimeoutException {
         for (String videoId : list) {
-            JavaYoutubeDownloader.run(videoId, "./videos");
+            VideoDownloader.download(videoId, "./videos");
         }
     }
 
+    //todo: parse input
+    public static String[] parseInput(String[] args){
+        return null;
+    }
+
+    public static boolean getInfo(String id, boolean isChannel) {
+        if (isChannel) {
+            // gets channel info
+            return true;
+        }
+        // gets video info
+        return true;
+    }
+
+    /*
+    todo: parse input
+    input:
+        * --info              - retrieves info about video or channel
+        * --download          - downloads video or all the videos
+        * --folder=[path]     - Folder where the videos are stored
+        * --video=[video Id]  - Downloads specific video to folder
+        * --channel=[channel] - Downloads all the videos from the channel
+     */
     public static void main(String[] args) throws TimeoutException {
+
         List<String> list = VideosDownloader.getAllVideos("ValerioVelardoTheSoundofAI");
         downloadVideos(list);
     }
